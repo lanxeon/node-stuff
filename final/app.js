b305c25cc38f4ca42ded9714cccc4ca0a1d83bce
lanxeon <lanxeon@users.noreply.github.com>
2020-02-11 10:07:46 +0530

    added an extra get for now

--- final/app.js
+++ final/app.js
@@ -3,13 +3,30 @@ const express = require('express');
 const app = express();
 const pool = require('./db/index');
 
-app.get('/monsters', (req, res) =>{
+app.get('/monsters', (req, res, next) =>{
     pool.query('select * from monsters order by mid asc', (err, result) => {
-        if(err) return console.log(err);
+        if(err) return next(err);
     
         res.json(result.rows);
-    });
+    }
+    );
 }
 );
+
+app.get('/monsters/:id', (req, res, next) =>{
+
+    const { id } = req.params;
+    console.log(id);
+
+    pool.query('select * from monsters where mid=$1',[id],(err,result) =>{
+        if(err) return next(err);
+
+        res.json(result.rows);
+    });
+});
+
+app.use((err,req,res,next) => {
+    res.json(err);
+});
 
 module.exports = app;
